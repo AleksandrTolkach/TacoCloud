2b5fa781a99fc1341e71bf57c16f0335f5ff5bcd
@a.toukach <shuuurick@mail.ru>
2023-08-12 22:18:25 +0300

    Chapter3: Add Spring Data JPA

--- src/main/java/tacos/Ingredient.java
+++ src/main/java/tacos/Ingredient.java
@@ -1,28 +1,21 @@
 package tacos;
 
-import lombok.AccessLevel;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.domain.Persistable;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
-@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
+@Entity
+@NoArgsConstructor
 @AllArgsConstructor
-public class Ingredient implements Persistable<String> {
+public class Ingredient {
 
   @Id
-  private final String id;
-  private final String name;
-  private final Type type;
-
-  @Override
-  public boolean isNew() {
-    return true;
-  }
+  private String id;
+  private String name;
+  private Type type;
 
   public enum Type {
     WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,18 +1,22 @@
 package tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
   private Date createdAt;
@@ -23,5 +27,10 @@ public class Taco {
 
   @NotNull
   @Size(min = 1, message = "You must choose at least 1 ingredient")
-  private List<IngredientRef> ingredients;
+  @ManyToMany
+  private List<Ingredient> ingredients;
+
+  public void addIngredient(Ingredient ingredient) {
+    this.ingredients.add(ingredient);
+  }
 }
